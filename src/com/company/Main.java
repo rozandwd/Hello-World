ebbab0467a3f235e561c4457a3a939610093bdca
rozandwd <73720154+rozandwd@users.noreply.github.com>
2021-01-05 17:43:25 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com..");
-        System.out.print("Sandesh Lamsal");
+        System.out.print("Roshan Dawadi");
 	//Branch1
 	
     }
